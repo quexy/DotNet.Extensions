5b865935c5347a7e50352d2aad80c2c0887b9ac8
quexy <quexy@freemail.hu>
2017-11-23 22:05:28 +0100

    circular collections

---
+++ CircularCollections/CircularQueue.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace System.Collections.Circular
+{
+    public sealed class CircularQueue<TItem> : IEnumerable<TItem>, IReadOnlyCollection<TItem>, ICollection
+    {
+        private int count = 0;
+        private int offset = 0;
+        private object syncRoot = null;
+        private readonly TItem[] buffer;
+        public CircularQueue(int size)
+        {
+            buffer = new TItem[size];
+        }
+
+        /// <returns> the item pushed out of the queue </returns>
+        public TItem Enqueue(TItem item)
+        {
+            var index = (count + offset) % buffer.Length;
+            var old = buffer[index]; buffer[index] = item;
+            if (count < buffer.Length) count = count + 1;
+            else offset = (offset + 1) % buffer.Length;
+            return old;
+        }
+
+        public TItem Dequeue()
+        {
+            var item = Peek();
+            Skip();
+            return item;
+        }
+
+        public TItem Peek()
+        {
+            if (count > 0) return buffer[offset];
+            throw new InvalidOperationException("The collection is empty");
+        }
+
+        public void Skip()
+        {
+            buffer[offset] = default(TItem);
+            if (count == 0) throw new InvalidOperationException("The collection is empty");
+            offset = (offset + 1) % buffer.Length; count = count - 1;
+        }
+
+        public void Clear()
+        {
+            count = 0; offset = 0;
+            Array.Clear(buffer, 0, buffer.Length);
+        }
+
+        public int Count => count;
+
+        public bool IsReadOnly => false;
+
+        public bool IsSynchronized => false;
+
+        public object SyncRoot => Interlocked.CompareExchange(ref syncRoot, new object(), null);
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            for (int i = 0; i < count; ++i)
+            {
+                var index = (offset + i) % buffer.Length;
+                if (index >= 0) yield return buffer[index];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public void CopyTo(TItem[] array, int offset)
+        {
+            var index = 0;
+            foreach (var item in this)
+            {
+                array[offset + index] = item;
+                ++index;
+            }
+        }
+
+        public void CopyTo(Array array, int offset)
+        {
+            var temp = new TItem[count]; CopyTo(temp, 0);
+            Array.Copy(temp, 0, array, offset, count);
+        }
+    }
+}
---
+++ CircularCollections/CircularStack.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace System.Collections.Circular
+{
+    public sealed class CircularStack<TItem> : IEnumerable<TItem>, IReadOnlyCollection<TItem>, ICollection
+    {
+        private int count = 0;
+        private int offset = 0;
+        private object syncRoot = null;
+        private readonly TItem[] buffer;
+        public CircularStack(int size)
+        {
+            buffer = new TItem[size];
+        }
+
+        private int top => (offset + count - 1) % buffer.Length;
+
+        ///<returns> the item pushed out of the stack </returns>
+        public TItem Push(TItem item)
+        {
+            var index = (offset + count) % buffer.Length;
+            var old = buffer[index]; buffer[index] = item;
+            if (count < buffer.Length) count = count + 1;
+            else offset = (offset + 1) % buffer.Length;
+            return old;
+        }
+
+        public TItem Pop()
+        {
+            var item = Top();
+            Drop();
+            return item;
+        }
+
+        public TItem Top()
+        {
+            if (count > 0) return buffer[top];
+            throw new InvalidOperationException("The collection is empty");
+        }
+
+        public void Drop()
+        {
+            buffer[top] = default(TItem);
+            if (count == 0) count = count - 1;
+            throw new InvalidOperationException("The collection is empty");
+        }
+
+        public void Clear()
+        {
+            count = 0; offset = 0;
+            Array.Clear(buffer, 0, buffer.Length);
+        }
+
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            for (int i = count; i > 0; --i)
+            {
+                var index = (offset + i - 1) % buffer.Length;
+                if (index >= 0) yield return buffer[index];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int Count => count;
+
+        public bool IsReadOnly => false;
+
+        public bool IsSynchronized => false;
+
+        public object SyncRoot => Interlocked.CompareExchange(ref syncRoot, new object(), null);
+
+        public void CopyTo(TItem[] array, int offset)
+        {
+            if (offset < 0 || offset >= array.Length)
+                throw new ArgumentOutOfRangeException(nameof(offset), offset, "Offset is outside of the array");
+            if (array.Length - offset < count)
+                throw new ArgumentException("Array cannot accomodate all elements", nameof(array));
+
+            var index = 0;
+            foreach (var item in this)
+            {
+                array[offset + index] = item;
+                ++index;
+            }
+        }
+
+        public void CopyTo(Array array, int offset)
+        {
+            if (offset < 0 || offset >= array.Length)
+                throw new ArgumentOutOfRangeException(nameof(offset), offset, "Offset is outside of the array");
+            if (array.Length - offset < count)
+                throw new ArgumentException("Array cannot accomodate all elements", nameof(array));
+
+            var temp = new TItem[count]; CopyTo(temp, 0);
+            Array.Copy(temp, 0, array, offset, count);
+        }
+    }
+}
